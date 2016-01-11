111ccbab150537ca1e514e1288b311c2d62f7315
Mike Delucchi <Mike.Delucchi@nike.com>
2016-01-11 15:21:43 -0800

    use stable node and karma-firefox-launcher

--- config/env/test.js
+++ config/env/test.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     plugins: [
       'karma-webpack',
-      'karma-coverage'
+      'karma-coverage',
+      'karma-firefox-launcher'
     ],
     autoWatch: false,
     singleRun: true
