c9a4122d34fe8408b22920e6e84306327f40bcd4
Mike Delucchi <Mike.Delucchi@nike.com>
2016-01-11 14:52:37 -0800

    added chrome travis ci config and before install setup

--- config/env/test.js
+++ config/env/test.js
@@ -8,6 +8,10 @@ module.exports = {
       dir: 'coverage/',
       type: 'lcov'
     },
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox', '--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"']
+    },
     plugins: [
       'karma-webpack',
       'karma-coverage',
