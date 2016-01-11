d941193573cf2effe0a632ccd4c9e1c6d55a44d7
Mike Delucchi <Mike.Delucchi@nike.com>
2016-01-11 15:10:22 -0800

    trying out firefox for travis ci tests

--- config/env/test.js
+++ config/env/test.js
@@ -2,20 +2,15 @@
 
 module.exports = {
   karma: {
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     reporters: ['spec', 'coverage', 'coveralls'],
     coverageReporter: {
       dir: 'coverage/',
       type: 'lcov'
     },
-    Chrome_travis_ci: {
-      base: 'Chrome',
-      flags: ['--no-sandbox', '--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"']
-    },
     plugins: [
       'karma-webpack',
-      'karma-coverage',
-      'karma-chrome-launcher'
+      'karma-coverage'
     ],
     autoWatch: false,
     singleRun: true
